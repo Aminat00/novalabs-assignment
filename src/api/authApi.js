95477235b9602d1506589e7a45d37b3a555f946e
Aminat Moldalieva <mldlv.mnt.kg@gmail.com>
2022-12-18 06:23:44 +0100

    FIrst commit

---
+++ src/api/authApi.js
@@ -0,0 +1,12 @@
+import axiosClient from "./axiosClient"
+
+const authApi = {
+	signup: params => {
+		console.log({ params })
+		return axiosClient.post("auth/signup", params)
+	},
+	login: params => axiosClient.post("auth/login", params),
+	verifyToken: () => axiosClient.post("auth/verify-token"),
+}
+
+export default authApi
---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+	images: {
+		logoRed: require("./images/kanban-logo.png"),
+		appIcon: require("./images/kanban-icon.png"),
+	},
+	colors: {
+		secondary: "#47A67577",
+	},
+}
+
+export default assets
